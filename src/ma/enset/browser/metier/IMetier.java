979e648f161f9cecf48a267f39ece9756d6956ec
Chaimae-Chakir <chaimaechakir24@gmail.com>
2023-02-26 15:17:04 +0100

    add metier interface

---
+++ src/ma/enset/browser/metier/IMetier.java
@@ -0,0 +1,28 @@
+package ma.enset.browser.metier;
+
+import ma.enset.browser.dao.entities.Favori;
+import ma.enset.browser.dao.entities.Historique;
+import ma.enset.browser.dao.entities.Page;
+import ma.enset.browser.dao.entities.Telechargement;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface IMetier {
+
+    void addPage(Page page);
+
+    List<Page> getAllPage();
+//    List<Historique> getAllHistorique();
+    List<Page> searchPage(String cle);
+    void deletePage(Page page);
+    void affectPageTofavorite(Favori favori, Page page) throws SQLException;
+    void affectPageToHistorical(Historique historique, Page page) throws SQLException;
+    void affectPageToDownload(Telechargement telechargement, Page page) throws SQLException;
+    List<Page> getPageByIdHistorique(LocalDate date);
+    void supprimerPageInHistorique(Page page);
+    void supprimerPageInFavoris (Page page);
+    void ajouterPagetoFavoris(Page page);
+
+}
