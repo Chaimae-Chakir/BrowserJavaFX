faecc3f5c6ebdc0b2a4b4621766c14e7211e91bb
Chaimae-Chakir <chaimaechakir24@gmail.com>
2023-02-26 15:10:12 +0100

    add a singletonConnexionDB

---
+++ src/ma/enset/browser/dao/SingletonConnexionDB.java
@@ -0,0 +1,20 @@
+package ma.enset.browser.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SignletonConnexionDB {
+
+    private static Connection connection;
+    static {
+        try {
+            Class.forName("com.mysql.c.Driver");
+            connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/browser","root","");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static Connection getConnection() {
+        return connection;
+    }
+}
